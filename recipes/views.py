5a3e1df0cdeee17c12d377311abe71605599be34
EliweltonMoreira <eliwelton.moreira@outlook.com>
2022-03-08 21:11:52 -0300

    add a new URL for a new view

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Eliwelton Moreira',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Eliwelton Moreira',
+    })
